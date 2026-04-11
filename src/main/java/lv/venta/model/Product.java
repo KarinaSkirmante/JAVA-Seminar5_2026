56808d26f895acd08f7878e50d30b3284e42af20
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-11 11:03:41 +0300

    Edit: added data JPA library and annotatins in the Product class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +16,32 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	//1. mainigie
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id //ka primara atslega
+	@GeneratedValue(strategy = GenerationType.AUTO)//tiks glabat DB automatiska pec ato increament algoritma
 	private long id;
+	
+	@Column(name = "Title")
 	private String title;
+	
+	@Column(name = "Price")
 	private float price;
+	
+	@Column(name = "Quantity")
 	private int quantity;
+	
+	@Column(name = "Description")
 	private String description;
+	
+	@Column(name = "ProductType")
 	private ProductType productType;
 	
 	
-	private static long counter = 0;
 	//2. getters - nak no lombok bibliotekas
 	//3. setters - nak no lombok bibliotekas
 	//4. abi konstruktori - bez argumenata konstruktors nak no lombok bibliotekas
@@ -33,7 +53,6 @@ public class Product {
 		setPrice(inputPrice);
 		setQuantity(inputQuantity);
 		setProductType(inputProductType);
-		id = counter++;
 		
 	}
 	//5. toString - nak no lombok bibliotekas
