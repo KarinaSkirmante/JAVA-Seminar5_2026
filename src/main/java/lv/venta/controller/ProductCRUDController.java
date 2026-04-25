a245c020d9d96f3447abd637eb33594d49c689a9
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-25 10:07:41 +0300

    Edit: added new controller for adding new product

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -56,6 +57,29 @@ public class ProductCRUDController {
 			model.addAttribute("package", prodFromDB);
 			return "show-one-product-page";
 			
+		}
+		catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("/add")//localhost:8080/product/crud/add
+	public String getAddNewProduct(Model model) {
+		//lai iveidotu jaunu produktu, iedodam noklsueto proukdu, kuru pec tam vares aizpildit html puse
+		model.addAttribute("product", new Product());
+		return "add-new-product-page";//tiks paradita add-new-product-page.html lapa
+	}
+	
+	@PostMapping("/add")
+	public String postAddNewProduct(Product product, Model model) {
+		try
+		{
+			prodService.create(product.getTitle(), product.getPrice(), product.getQuantity(),
+				product.getDescription(), product.getProductType());
+		
+			//ja ir redirect, tad uz url adresi parmet (ne lapu)
+			return "redirect:/product/crud/all";
 		}
 		catch (Exception e) {
 			model.addAttribute("package", e.getMessage());
