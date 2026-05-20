b83c97085556a943edc6278b8f7a9fb959e58e00
KarinaSkirmante <karina.krinkele@gmail.com>
2026-05-20 10:24:45 +0300

    fix: MyAuthority

--- src/main/java/lv/venta/model/MyAuthority.java
+++ src/main/java/lv/venta/model/MyAuthority.java
@@ -1,7 +1,15 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,10 +22,21 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "MyAuthorityTable")
+@Entity
 public class MyAuthority {
 	
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Ida")
+	@Id //ka primara atslega
+	@GeneratedValue(strategy = GenerationType.AUTO)//tiks glabat DB automatiska pec ato increament algoritma
 	private long ida;
 	
+	@Column(name = "Title")
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Z_]{4,10}")
 	private String title;
+	
+	
 }
